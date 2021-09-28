62c70d817a5c7ae81a50863bc46880ace6632c16
Salman Malik <salman@the-tech-warriors.com>
2021-09-27 20:04:28 -0400

    requester now works

--- requester/src/main/java/com/smalik/choreographer/MainController.java
+++ requester/src/main/java/com/smalik/choreographer/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     private final RequesterService service;
+    private final WebClient webClient;
 
     @PostMapping
     public void turn(@RequestBody Load request) {
-        service.generateLoad(request);
+        service.generateLoad(webClient, request);
     }
 }
--- requester/src/main/java/com/smalik/choreographer/RequesterService.java
+++ requester/src/main/java/com/smalik/choreographer/RequesterService.java
@@ -1,17 +1,70 @@
 package com.smalik.choreographer;
 
+import com.github.javafaker.Faker;
+import com.smalik.choreographer.api.TurnRequest;
+import com.smalik.choreographer.api.TurnResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RequesterService {
 
-    private final WebClient webClient;
+    private final Faker faker = Faker.instance();
 
-    public void generateLoad(Load load) {
-        Flux.
+    public void generateLoad(WebClient webClient, Load load) {
+        AtomicInteger counter = new AtomicInteger(0);
+        Flux.range(1, load.getDurationSeconds())
+                .limitRate(1)
+                .delayElements(Duration.ofSeconds(1))
+                .map(idx -> Flux.range(1, load.getArrivalRate()))
+                .flatMap(idx -> idx)
+                .map(idx -> generateRequest())
+                .map(req -> makeRequest(webClient, req))
+                .flatMap(resp -> resp)
+                .subscribe(resp -> collectStats(counter, resp));
+    }
+
+    private void collectStats(AtomicInteger counter, TurnResponse resp) {
+        log.info("Number={}, Id={}, ResponseGenerationTime={}, ResponseTime={}",
+                counter.incrementAndGet(),
+                resp.getTurnId(),
+                Duration.between(resp.getStartTime(), resp.getFinishTime()).toMillis(),
+                Duration.between(resp.getStartTime(), OffsetDateTime.now()).toMillis());
+    }
+
+    private TurnRequest generateRequest() {
+        return TurnRequest.builder()
+                .time(OffsetDateTime.now())
+                .playerId(faker.lorem().word())
+                .turnId(UUID.randomUUID().toString())
+                .moves(List.of(TurnRequest.MoveRequest.builder()
+                        .moveId(UUID.randomUUID().toString())
+                        .places(4)
+                        .type("type")
+                        .build()))
+                .build();
+    }
+
+    private Mono<TurnResponse> makeRequest(WebClient webClient, TurnRequest request) {
+        return webClient.post()
+                .uri("/turns")
+                .body(BodyInserters.fromValue(request))
+                .retrieve()
+                .bodyToMono(TurnResponse.class)
+                .publishOn(Schedulers.boundedElastic());
     }
 }
