6d2c8f6fd22604297eb1c04480e6963bf915d88f
Salman Malik <salman@the-tech-warriors.com>
2021-10-02 15:42:13 -0400

    Updating the load testing script

--- load-test.sh
+++ load-test.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
 LOAD='[
-  {
-    "arrivalRate": 5,
-    "durationSeconds": 30
-  },
-  {
-    "arrivalRate": 25,
-    "durationSeconds": 30
-  },
-  {
-    "arrivalRate": 50,
-    "durationSeconds": 30
-  },
-  {
-    "arrivalRate": 75,
-    "durationSeconds": 60
-  },
-  {
-    "arrivalRate": 100,
-    "durationSeconds": 60
-  },
-  {
-    "arrivalRate": 100,
-    "durationSeconds": 60
-  },
-  {
-    "arrivalRate": 125,
-    "durationSeconds": 120
-  }
+  { "arrivalRate": 5,   "durationSeconds": 15 },
+  { "arrivalRate": 10,  "durationSeconds": 15 },
+  { "arrivalRate": 20,  "durationSeconds": 15 },
+  { "arrivalRate": 30,  "durationSeconds": 15 },
+  { "arrivalRate": 40,  "durationSeconds": 15 },
+  { "arrivalRate": 50,  "durationSeconds": 15 },
+  { "arrivalRate": 60,  "durationSeconds": 15 },
+  { "arrivalRate": 70,  "durationSeconds": 15 },
+  { "arrivalRate": 80,  "durationSeconds": 15 },
+  { "arrivalRate": 90,  "durationSeconds": 15 },
+  { "arrivalRate": 100, "durationSeconds": 15 },
+  { "arrivalRate": 110, "durationSeconds": 15 },
+  { "arrivalRate": 120, "durationSeconds": 15 },
+  { "arrivalRate": 130, "durationSeconds": 15 },
+  { "arrivalRate": 140, "durationSeconds": 15 },
+  { "arrivalRate": 150, "durationSeconds": 15 },
+  { "arrivalRate": 160, "durationSeconds": 15 },
+  { "arrivalRate": 170, "durationSeconds": 15 },
+  { "arrivalRate": 180, "durationSeconds": 15 },
+  { "arrivalRate": 190, "durationSeconds": 15 },
+  { "arrivalRate": 200, "durationSeconds": 15 },
+  { "arrivalRate": 210, "durationSeconds": 15 },
+  { "arrivalRate": 220, "durationSeconds": 15 },
+  { "arrivalRate": 230, "durationSeconds": 15 },
+  { "arrivalRate": 240, "durationSeconds": 15 },
+  { "arrivalRate": 250, "durationSeconds": 15 }
 ]'
 
 curl -X POST 'http://localhost:8181/requests' -H 'Content-Type: application/json' -d "${LOAD}"
