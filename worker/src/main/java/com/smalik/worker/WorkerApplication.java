fd53c5b175c8c02935766fdd9cb82a1b237d1160
Salman Malik <salman@the-tech-warriors.com>
2021-10-06 12:06:00 -0400

    Adding some logging to the worker

--- worker/src/main/java/com/smalik/worker/WorkerApplication.java
+++ worker/src/main/java/com/smalik/worker/WorkerApplication.java
@@ -1,5 +1,6 @@
 package com.smalik.worker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import java.util.Random;
 import java.util.function.Function;
 
 @SpringBootApplication
+@Slf4j
 public class WorkerApplication {
 
 	public static void main(String[] args) {
@@ -17,12 +19,15 @@ public class WorkerApplication {
 
 	@Bean
 	public Function<MoveStepRequest, MoveStepResponse> processor() {
-		return request -> MoveStepResponse.builder()
+		return request -> {
+			log.info("Got request: Player={}, Move={}, Turn={}, Step={}", request.getPlayerId(), request.getMoveId(), request.getTurnId(), request.getStep());
+			return MoveStepResponse.builder()
 					.moveId(request.getMoveId())
 					.playerId(request.getPlayerId())
 					.turnId(request.getTurnId())
 					.failed(false)
 					.time(OffsetDateTime.now())
 					.build();
+		};
 	}
 }
